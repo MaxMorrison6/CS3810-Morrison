cb2ba6cb11f824825679904a665569d3298ad65f
Max Morrison <31318856+MaxMorrison6@users.noreply.github.com>
2017-10-24 10:45:03 -0600

    Delete Homework3Load.sql

--- HW3/Homework3Load.sql
+++
@@ -1,9 +0,0 @@
-COPY Carriers FROM 'C:/Users/Max/Desktop/School/Semester 4 Fall 2017/CS 3810/HW3/CarrierTable.csv' WITH (FORMAT csv);
-
-COPY Destinations FROM 'C:/Users/Max/Desktop/School/Semester 4 Fall 2017/CS 3810/HW3/DestinationTable.csv' WITH (FORMAT csv);
-
-COPY Origins FROM 'C:/Users/Max/Desktop/School/Semester 4 Fall 2017/CS 3810/HW3/OriginTable.csv' WITH (FORMAT csv);
-
-COPY FlightInfo FROM 'C:/Users/Max/Desktop/School/Semester 4 Fall 2017/CS 3810/HW3/FlightTable.csv' WITH (FORMAT csv);
-
-COPY AircraftInfo FROM 'C:/Users/Max/Desktop/School/Semester 4 Fall 2017/CS 3810/HW3/AircraftInfo.csv' WITH (FORMAT csv);
